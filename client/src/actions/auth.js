91ab0b89f8afca644693ec897a4f8bc6d2c0eb4a
Lucas Zanek <lucaszanek94@gmail.com>
2020-03-01 19:53:05 -0300

    Auth reducer working, registering user and saving token to local storage.

---
+++ client/src/actions/auth.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+import { REGISTER_SUCCESS, REGISTER_FAIL } from "./types";
+import { setAlert } from "./alert";
+
+//* REGISTER USER
+
+export const register = ({ name, email, password }) => async dispatch => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+
+  const body = JSON.stringify({ name, email, password });
+
+  try {
+    const res = await axios.post(
+      "http://localhost:5000/api/users",
+      body,
+      config
+    );
+    console.log("sigo aca");
+    if (res.status === 400) {
+    }
+    dispatch({
+      type: REGISTER_SUCCESS,
+      payload: res.data
+    });
+  } catch (error) {
+    console.log("error");
+    const errors = error.response.data.errors;
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, "danger")));
+    }
+
+    dispatch({
+      type: REGISTER_FAIL
+    });
+  }
+};
